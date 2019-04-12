2c20a95cdd2ac7a1bcfe79f948f0882a248dad5b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-12 12:45:52 +0000

    Deploy icebergMNE/people-counting-dashboard to github.com/icebergMNE/people-counting-dashboard.git:gh-pages

---
+++ precache-manifest.97438df70f30cf90d040e3b0f82cdede.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "04b4a4a95db777636f0a",
+    "url": "/people-counting-dashboard/static/css/main.7cfeac00.chunk.css"
+  },
+  {
+    "revision": "04b4a4a95db777636f0a",
+    "url": "/people-counting-dashboard/static/js/main.04b4a4a9.chunk.js"
+  },
+  {
+    "revision": "dee63502dfd6d1f2df61",
+    "url": "/people-counting-dashboard/static/js/1.dee63502.chunk.js"
+  },
+  {
+    "revision": "28b2ccb15d1961f404c8",
+    "url": "/people-counting-dashboard/static/js/runtime~main.28b2ccb1.js"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/people-counting-dashboard/static/media/react-logo.eb6be414.png"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/people-counting-dashboard/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "9fcf69e5359987c9b1545aae1c2c5faa",
+    "url": "/people-counting-dashboard/static/media/emilyz.9fcf69e5.jpg"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/people-counting-dashboard/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/people-counting-dashboard/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/people-counting-dashboard/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/people-counting-dashboard/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "b7b3c55ffc1554cbfe0600f72ace4766",
+    "url": "/people-counting-dashboard/index.html"
+  }
+];
